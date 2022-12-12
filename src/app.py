b5146db9cbbb15dbd2dcafc43f299e8432a4d1ce
jose <josedelvalle012@gmail.com>
2022-12-12 18:39:05 -0300

    Creo ruta delete

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -10,6 +11,9 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS'] = UPLOADS
 
 mysql.init_app(app)
 
@@ -54,6 +58,59 @@ def store():
 
     return redirect('/')
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    sql = f'DELETE FROM empleados WHERE id={id}'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
+    return redirect('/')
+
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql = f'DELETE FROM empleados WHERE id={id}'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    empleado = cursor.fetchone()
+    conn.commit() 
+    return render_template('empleados/edit.html', empleado=empleado)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _nombre = request.form['txtCorreo']
+    _nombre = request.form['txtFoto']
+    id = request.form['txtId']
+
+    datos = (_nombre, _correo, id)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if _foto.filename != '':
+        now = datetime.now()
+        tiempo = now.strftime("%Y%H%S")
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
+    sql = 'SELECT foto FROM empleados WHERE id={id}'
+    cursor.execute(sql)
+
+    nombreFoto = cursor.fetchone()[0]
+
+    os.remove(os.path.join(app.config['UPLOADS'], nombreFoto))
+
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    sql = f'UPDATE empleados SET nombre={_nombre}, correo={_correo} WHERE id={id}'
+    cursor.execute(sql)
+    conn.commit()
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
